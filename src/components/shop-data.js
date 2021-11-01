e920e219c2b128bb68e4c2ae38e75f29ca8fd4d4
Fernanda Cherini <fer.azvdo@gmail.com>
2021-11-01 17:48:14 -0300

    Build

---
+++ src/components/shop-data.js
@@ -0,0 +1,12 @@
+const shop_data = [
+  {
+    id: "1",
+    name: "Savage Shirt",
+    info: "Shirt with Savage Print",
+    image:
+      "https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg",
+    price: "19,99",
+    discount: "50%",
+    category: "top",
+  },
+];
